8d8a98ab49a9a4959477b6f676bf8294cc42a237
VVsxmja <vvsxmja@qq.com>
2022-11-27 19:42:47 +0800

    update

--- Frontend/src/components/todolist.vue
+++ Frontend/src/components/todolist.vue
@@ -45,9 +45,10 @@
         <el-container class="todolist-main" direction="vertical">
 
             <el-container class="todolist-top-itemadder" direction="horizontal">
-                <el-input type="text" placeholder="在此处添加新的待办事项" id="todolist-top-itemadder-input" />
+                <el-input type="text" placeholder="在此处添加新的待办事项" id="todolist-top-itemadder-input"
+                    v-model="this.newEventBrief" clearable @keypress.enter="CreateEventFromBrief" />
                 <el-divider direction="vertical" style="height: 100%; border: none;" />
-                <el-button type="primary">
+                <el-button type="primary" @click="CreateEventFromBrief">
                     <el-icon color="#FFFFFF">
                         <Plus />
                     </el-icon>
@@ -70,7 +71,8 @@
                                             <el-space style="display: flex; flex-flow: row nowrap; align-items: center">
                                                 <el-checkbox class="todolist-item-checkbox" v-model="ev.finished">
                                                 </el-checkbox>
-                                                <p style="flex: 1 0 auto; margin: 0px;">{{ ev.brief }}</p>
+                                                <p style="flex: 1 0 auto; margin: 0px; overflow: hidden;">{{ ev.brief }}
+                                                </p>
                                                 <el-tag v-if="ev.importance.important">重要</el-tag>
                                                 <el-tag v-if="ev.importance.urgent">紧急</el-tag>
                                             </el-space>
@@ -87,28 +89,52 @@
         <div class="todolist-detail-right-background">
             <el-container class="todolist-detail" v-if="this.chosenEventID" direction="vertical">
                 <el-space class="todolist-detail-container" direction="vertical" :fill="true">
-                    <input v-model="chosenEvent.brief" id="todolist-detail-brief" />
+                    <input v-model="chosenEvent.brief" id="todolist-detail-brief" placeholder="无标题" />
                 </el-space>
                 <el-space class="todolist-detail-container" direction="horizontal">
                     <p style="font-weight: bold; margin: 0; width: 100px;">重要程度：</p>
                     <el-button plain :type="this.chosenEvent.importance.important ? 'primary' : ''"
                         :class="{ active: this.chosenEvent.importance.important }" style="width:100px;"
-                        @click="this.chosenEvent.importance.important = !this.chosenEvent.importance.important">{{
-                                this.chosenEvent.importance.important ? '重要' : '不重要'
-                        }}
+                        @click="this.chosenEvent.importance.important = !this.chosenEvent.importance.important">
+                        {{ this.chosenEvent.importance.important ? '重要' : '不重要' }}
                     </el-button>
                     <el-button plain :type="this.chosenEvent.importance.urgent ? 'primary' : ''"
                         :class="{ active: this.chosenEvent.importance.urgent }" style="width:100px;"
-                        @click="this.chosenEvent.importance.urgent = !this.chosenEvent.importance.urgent">{{
-        this.chosenEvent.importance.urgent ? '紧急' : '不紧急'
-                        }}
+                        @click="this.chosenEvent.importance.urgent = !this.chosenEvent.importance.urgent">
+                        {{ this.chosenEvent.importance.urgent ? '紧急' : '不紧急' }}
                     </el-button>
                 </el-space>
                 <el-space class="todolist-detail-container" direction="horizontal">
-                    <p style="font-weight: bold; margin: 0; width: 100px;">开始时间：</p>
+                    <el-button text bg v-if="!this.chosenEvent.time.beginTime.date"
+                        @click="this.chosenEvent.time.beginTime.date = new Date()">添加{{ this.beginTimeTag }}日期
+                    </el-button>
+                    <template v-else>
+                        <p style="font-weight: bold; margin: 0; width: 100px;">
+                            {{ this.beginTimeTag }}时间：
+                        </p>
+                        <el-date-picker v-model="this.chosenEvent.time.beginTime.date"></el-date-picker>
+                        <el-button text bg v-if="!this.chosenEvent.time.beginTime.time"
+                            @click="this.chosenEvent.time.beginTime.time = new Date()">添加{{ this.beginTimeTag }}时间
+                        </el-button>
+                        <el-time-picker v-else v-model="this.chosenEvent.time.beginTime.time"></el-time-picker>
+                    </template>
                 </el-space>
-                <el-space class="todolist-detail-container" direction="horizontal">
-                    <p style="font-weight: bold; margin: 0; width: 100px;">结束时间：</p>
+                <el-space class="todolist-detail-container" direction="horizontal"
+                    v-if="this.chosenEvent.time.beginTime.date">
+                    <el-button text bg v-if="!this.chosenEvent.time.endTime.date"
+                        @click="this.chosenEvent.time.endTime.date = new Date()">添加结束日期
+                    </el-button>
+                    <template v-else>
+                        <p style="font-weight: bold; margin: 0; width: 100px;">结束时间：</p>
+                        <el-date-picker v-model="this.chosenEvent.time.endTime.date"></el-date-picker>
+                        <el-button text bg v-if="!this.chosenEvent.time.endTime.time"
+                            @click="this.chosenEvent.time.endTime.time = new Date()">添加结束时间
+                        </el-button>
+                        <el-time-picker v-else v-model="this.chosenEvent.time.endTime.time"></el-time-picker>
+                    </template>
+                </el-space>
+                <el-space class="todolist-detail-container" direction="horizontal" :fill="true">
+                    <el-input type="textarea" placeholder="详细信息" v-model="this.chosenEvent.description" />
                 </el-space>
             </el-container>
         </div>
@@ -119,7 +145,35 @@
   
 <script>
 
-// import { v4 as uuid } from 'uuid'
+import { v4 as uuid } from 'uuid'
+
+function compareDate(a, b) {
+    a = new Date(a), b = new Date(b)
+    if (a.getFullYear() != b.getFullYear()) return a.getFullYear() < b.getFullYear() ? -1 : 1
+    if (a.getMonth() != b.getMonth()) return a.getMonth() < b.getMonth() ? -1 : 1
+    if (a.getDate() != b.getDate()) return a.getDate() < b.getDate() ? -1 : 1
+    return 0
+}
+
+function compareTime(a, b) {
+    a = new Date(a), b = new Date(b)
+    if (a.getHours() != b.getHours()) return a.getHours() < b.getHours() ? -1 : 1
+    if (a.getMinutes() != b.getMinutes()) return a.getMinutes() < b.getMinutes() ? -1 : 1
+    if (a.getSeconds() != b.getSeconds()) return a.getSeconds() < b.getSeconds() ? -1 : 1
+    if (a.getMilliseconds() != b.getMilliseconds()) return a.getMilliseconds() < b.getMilliseconds() ? -1 : 1
+    return 0
+}
+
+function CompareDateTime(a, b) {
+    let dateResult = compareDate(a.date, b.date)
+    if (!a.time || !b.time || dateResult !== 0) return dateResult
+    return compareTime(a.time, b.time)
+}
+
+function CheckDateTime(x) {
+    if (!x) return false
+    return !!x.date
+}
 
 let GroupFilters = [
     {
@@ -132,10 +186,15 @@ let GroupFilters = [
     {
         groupName: '已过期',
         filterFunction(ev) {
-            if (ev.time.endTime) {
-                return ev.time.endTime < new Date()
-            } else if (ev.time.beginTime) {
-                return ev.time.beginTime < new Date()
+            let curDateTime = {
+                date: new Date(),
+                time: new Date(),
+            }
+            if (!ev.time) return false
+            if (CheckDateTime(ev.time.endTime)) {
+                return CompareDateTime(curDateTime, ev.time.endTime) === 1
+            } else if (CheckDateTime(ev.time.beginTime)) {
+                return CompareDateTime(curDateTime, ev.time.beginTime) === 1
             } else {
                 return false
             }
@@ -144,29 +203,31 @@ let GroupFilters = [
     {
         groupName: '进行中',
         filterFunction(ev) {
-            if (!ev.time.beginTime || !ev.time.endTime) return false
-            let currentTime = new Date()
-            return ev.time.beginTime <= currentTime && currentTime <= ev.time.endTime
+            if (!ev.time) return false
+            if (!CheckDateTime(ev.time.beginTime) || !CheckDateTime(ev.time.endTime)) return false
+            let curDateTime = {
+                date: new Date(),
+                time: new Date(),
+            }
+            return CompareDateTime(ev.time.beginTime, curDateTime) !== 1
+                || CompareDateTime(curDateTime, ev.time.endTime) !== 1
         }
     },
     {
         groupName: '今天',
         filterFunction(ev) {
-            if (!ev.time.beginTime) return false
-            let evTime = new Date(ev.time.beginTime), currentTime = new Date()
-            if (evTime.getFullYear() != currentTime.getFullYear()) return false
-            if (evTime.getMonth() != currentTime.getMonth()) return false
-            return (new Date(ev.time.beginTime)).getDate() == (new Date()).getDate()
+            if (!ev.time) return false
+            if (!CheckDateTime(ev.time.beginTime)) return false
+            return compareDate(ev.time.beginTime.date, new Date()) === 0
         }
     },
     {
         groupName: '近期',
         filterFunction(ev) {
-            if (!ev.time.beginTime) return false
-            let evTime = new Date(ev.time.beginTime), currentTime = new Date()
-            if (evTime.getFullYear() != currentTime.getFullYear()) return false
-            if (evTime.getMonth() != currentTime.getMonth()) return false
-            return (new Date(ev.time.beginTime)).getDate() <= (new Date()).getDate() + 7
+            if (!ev.time) return false
+            if (!CheckDateTime(ev.time.beginTime)) return false
+            let evDate = new Date(ev.time.beginTime.date), next7day = (new Date()).setDate((new Date()).getDate() + 7)
+            return compareDate(new Date(), evDate) === -1 && compareDate(evDate, next7day) === -1
         }
     }
 ]
@@ -177,12 +238,13 @@ export default {
     name: 'ToDoList',
     data() {
         return {
+            newEventBrief: '',
             openedGroups: [],
             chosenEventID: undefined,
             EventList: [
                 {
                     id: '0f819113-1b91-48cf-bddc-9179c35689f6',
-                    brief: '测试事项 1 普通',
+                    brief: '测试事项 1 今天整天',
                     finished: false,
                     description: '第一个测试事件',
                     importance: {
@@ -190,6 +252,9 @@ export default {
                         urgent: false,
                     },
                     time: {
+                        beginTime: {
+                            date: new Date(),
+                        },
                     }
                 },
                 {
@@ -202,12 +267,14 @@ export default {
                         urgent: false,
                     },
                     time: {
-                        beginTime: (new Date).setDate((new Date()).getDate() + 1),
+                        beginTime: {
+                            date: (new Date()).setDate((new Date()).getDate() + 1),
+                        },
                     }
                 },
                 {
                     id: 'f1cfa4f3-f164-46f8-b5fb-33e61f457a0e',
-                    brief: '测试事项 3 过期未完成',
+                    brief: '测试事项 3 今天过期',
                     finished: false,
                     description: '第三个测试事件',
                     importance: {
@@ -215,20 +282,26 @@ export default {
                         urgent: true,
                     },
                     time: {
-                        beginTime: new Date(1926, 8, 17),
+                        beginTime: {
+                            date: new Date(),
+                            time: new Date((new Date()).setHours((new Date()).getHours() - 1))
+                        },
                     }
                 },
                 {
                     id: 'cc5856a2-1c84-41ca-a11c-2d18517e03b1',
-                    brief: '测试事项 4 已完成',
-                    finished: true,
+                    brief: '测试事项 4 今天将来',
+                    finished: false,
                     description: '第四个测试事件',
                     importance: {
                         important: false,
                         urgent: true,
                     },
                     time: {
-                        beginTime: new Date(2077, 1, 1),
+                        beginTime: {
+                            date: new Date(),
+                            time: new Date((new Date()).setHours((new Date()).getHours() + 1))
+                        },
                     }
                 },
                 {
@@ -241,8 +314,12 @@ export default {
                         urgent: false,
                     },
                     time: {
-                        beginTime: new Date(1926, 8, 17),
-                        endTime: new Date(2077, 1, 1),
+                        beginTime: {
+                            date: new Date(1926, 8, 17),
+                        },
+                        endTime: {
+                            date: new Date(2077, 1, 1),
+                        },
                     }
                 },
             ],
@@ -251,14 +328,73 @@ export default {
     },
     watch: {
         chosenEventID() {
-            if (!this.chosenEventID) return undefined
             this.chosenEvent = this.EventList.find(ev => ev.id === this.chosenEventID)
+            this.chosenEvent.brief = this.chosenEvent.brief ?? null
+            this.chosenEvent.description = this.chosenEvent.description ?? null
+            this.chosenEvent.importance = this.chosenEvent.importance ?? {}
+            this.chosenEvent.importance.important = this.chosenEvent.importance.important ?? false
+            this.chosenEvent.importance.urgent = this.chosenEvent.importance.urgent ?? false
+            this.chosenEvent.time = this.chosenEvent.time ?? {}
+            this.chosenEvent.time.beginTime = this.chosenEvent.time.beginTime ?? {}
+            this.chosenEvent.time.beginTime.date = this.chosenEvent.time.beginTime.date ?? null
+            this.chosenEvent.time.beginTime.time = this.chosenEvent.time.beginTime.time ?? null
+            this.chosenEvent.time.endTime = this.chosenEvent.time.endTime ?? {}
+            this.chosenEvent.time.endTime.date = this.chosenEvent.time.endTime.date ?? null
+            this.chosenEvent.time.endTime.time = this.chosenEvent.time.endTime.time ?? null
         },
-        testTime() {
-            console.log(this.testTime)
-        }
+        eventGrouped(current, former) {
+            let currentGroups = current.map(group => group.groupName)
+            let formerGroups = former.map(group => group.groupName)
+            this.openedGroups = this.openedGroups.concat(currentGroups.filter(group => !formerGroups.includes(group)))
+            let groupsWithNewEvent = []
+            for (let groupNow of current) {
+                let groupFormer = former.find(group => group.groupName === groupNow.groupName)
+                if (!groupFormer) continue
+                let newEvents = groupNow.events.filter(ev => !groupFormer.events.map(ev => ev.id).includes(ev.id))
+                if (newEvents.length === 0) continue
+                groupsWithNewEvent.push(groupNow.groupName)
+            }
+            this.openedGroups = this.openedGroups.concat(groupsWithNewEvent)
+        },
+        openedGroups(current, former) {
+            let closedGroups = former.filter(group => !current.map(cg => cg).includes(group))
+            if (this.eventGrouped
+                .filter(group => closedGroups.includes(group.groupName))
+                .find(group => group.events.find(ev => ev.id === this.chosenEventID))) {
+                // 刚刚被折叠的分组中，包含当前正在查看的事件
+                this.chosenEventID = undefined
+            }
+        },
+        'chosenEvent.time.beginTime.date': function () {
+            if (!this.chosenEvent.time.beginTime.date) this.chosenEvent.time.beginTime = {}
+        },
+        'chosenEvent.time.endTime.date': function () {
+            if (!this.chosenEvent.time.endTime.date) this.chosenEvent.time.endTime = {}
+        },
+        'chosenEvent.description': function (val) {
+            console.warn(val, val.length, typeof val)
+        },
+        'chosenEvent.brief': function (val) {
+            console.warn(val, val.length, typeof val)
+        },
     },
     methods: {
+        CreateEventFromBrief() {
+            let newEvent = {
+                id: uuid(),
+                brief: this.newEventBrief ?? '',
+                description: '',
+                finished: false,
+                importance: {
+                    important: false,
+                    urgent: false,
+                },
+                time: undefined
+            }
+            this.EventList.push(newEvent)
+            this.chosenEventID = newEvent.id
+            this.newEventBrief = ''
+        },
 
     },
     computed: {
@@ -287,6 +423,9 @@ export default {
             }
             return grouped.concat(grouped_reverse.reverse())
         },
+        beginTimeTag() {
+            return CheckDateTime(this.chosenEvent.time.endTime) ? '开始' : ''
+        }
     },
     created() {
         this.openedGroups = this.eventGrouped.map(group => group.groupName) // 默认展开所有事件组
