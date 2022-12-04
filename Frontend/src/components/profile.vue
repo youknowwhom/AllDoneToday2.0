5174a0e40ac0d52a73a03c12fa2f09d1568d0475
youknowwhom <2154312@tongji.edu.cn>
2022-12-04 23:39:26 +0800

    update

--- Frontend/src/components/profile.vue
+++ Frontend/src/components/profile.vue
@@ -1,108 +1,116 @@
 <template>
-    <el-container class="profile-background"> 
+    <el-container class="profile-background">
         <div class="profile-header">
             <h2 class="header-title">个人设置</h2>
-            <el-button class="header-button" @click="(modify = !modify)" :disabled="filter=='safety-setting'" :type="modify?'primary':'basic'">
-                <p v-if="modify==true">保存设置</p>
+            <el-button class="header-button" @click="update" :disabled="filter=='safety-setting'"
+                :type="modify?'primary':'basic'">
+                <p v-if="modify == true">保存设置</p>
                 <p v-else>修改信息</p>
             </el-button>
         </div>
         <div class="profile-container">
-            <el-aside class = "profile-aside">
-                <el-menu default-active="1" class="el-menu-vertical-demo profile-menu" >
-                    <el-menu-item index="1" @click="filter='personal-info'" class="profile-menu-item">
+            <el-aside class="profile-aside">
+                <el-menu default-active="1" class="el-menu-vertical-demo profile-menu">
+                    <el-menu-item index="1" @click="filter = 'personal-info'" class="profile-menu-item">
                         <span slot="title">个人信息</span>
                     </el-menu-item>
-                    <el-menu-item index="2" @click="filter='safety-setting'" class="profile-menu-item">
+                    <el-menu-item index="2" @click="filter = 'safety-setting'" class="profile-menu-item">
                         <span slot="title">安全设置</span>
                     </el-menu-item>
                 </el-menu>
             </el-aside>
-            <el-main v-if="filter=='personal-info'" class="profile-main">
+            <el-main v-if="filter == 'personal-info'" class="profile-main">
                 <div class="avatar-username">
-                    <el-avatar :size="50"> {{username}} </el-avatar>
-                    <h2> {{username}} </h2>
+                    <el-avatar :size="50"> {{ username }} </el-avatar>
+                    <h2> {{ username }} </h2>
                 </div>
                 <div class="info-item">
-                    <el-col :span="6"><p class="info-item-title"> 个性签名 </p></el-col>
+                    <el-col :span="6">
+                        <p class="info-item-title"> 个性签名 </p>
+                    </el-col>
                     <el-col :span="12">
-                        <p v-if="!modify"> {{signature}} </p>
+                        <p v-if="!modify"> {{ signature }} </p>
                         <el-input v-else v-model="signature" placeholder="在此输入你的个性签名"></el-input>
-                    </el-col>                  
+                    </el-col>
                 </div>
                 <div class="info-item">
-                    <el-col :span="6"><p class="info-item-title"> 性别 </p></el-col>
+                    <el-col :span="6">
+                        <p class="info-item-title"> 性别 </p>
+                    </el-col>
                     <el-col :span="12">
-                        <p v-if="!modify"> {{gender}} </p>
+                        <p v-if="!modify"> {{ gender }} </p>
                         <el-select v-else v-model="gender" placeholder="在此选择性别">
-                            <el-option
-                            v-for="item in ['男', '女']"
-                            :key="item"
-                            :value="item">
+                            <el-option v-for="item in ['男', '女']" :key="item" :value="item">
                             </el-option>
                         </el-select>
-                    </el-col>                    
+                    </el-col>
                 </div>
                 <div class="info-item">
-                    <el-col :span="6"><p class="info-item-title"> 生日 </p></el-col>
+                    <el-col :span="6">
+                        <p class="info-item-title"> 生日 </p>
+                    </el-col>
                     <el-col :span="12">
-                        <p v-if="!modify"> {{getBirthday}} </p>
-                        <el-date-picker v-else
-                            v-model="birthday"
-                            type="date"
-                            placeholder="在此选择生日">
+                        <p v-if="!modify"> {{ getBirthday }} </p>
+                        <el-date-picker v-else v-model="birthday" type="date" placeholder="在此选择生日">
                         </el-date-picker>
-                    </el-col>                       
+                    </el-col>
                 </div>
                 <div class="info-item">
-                    <el-col :span="6"><p class="info-item-title"> 邮箱 </p></el-col>
+                    <el-col :span="6">
+                        <p class="info-item-title"> 邮箱 </p>
+                    </el-col>
                     <el-col :span="12">
-                        <p v-if="!modify"> {{email}} </p>
+                        <p v-if="!modify"> {{ email }} </p>
                         <el-tag v-else type="primary">请在安全设置中重置邮箱</el-tag>
-                    </el-col>                
+                    </el-col>
                 </div>
             </el-main>
-            <el-main v-else-if="filter=='safety-setting'" class="profile-main">
+            <el-main v-else-if="filter == 'safety-setting'" class="profile-main">
                 <div class="avatar-username">
-                    <el-avatar :size="50"> {{username}} </el-avatar>
-                    <h2> {{username}} </h2>
+                    <el-avatar :size="50"> {{ username }} </el-avatar>
+                    <h2> {{ username }} </h2>
                 </div>
                 <div class="info-item">
-                    <el-col :span="6"><p class="info-item-title"> 邮箱 </p></el-col>
+                    <el-col :span="6">
+                        <p class="info-item-title"> 邮箱 </p>
+                    </el-col>
                     <el-col :span="12">
-                        <p> {{email}} </p>
-                    </el-col>       
+                        <p> {{ email }} </p>
+                    </el-col>
                 </div>
                 <div class="info-item">
-                    <el-col :span="6"><p class="info-item-title"> 密码 </p></el-col>
+                    <el-col :span="6">
+                        <p class="info-item-title"> 密码 </p>
+                    </el-col>
                     <el-col :span="12">
                         <p>已设置</p>
                     </el-col>
                     <el-col :span="6">
-                    <el-button :type="primary" @click="this.$router.push('/resetPassword')">修改</el-button>     
-                    </el-col>      
+                        <el-button :type="primary" @click="this.$router.push('/resetPassword')">修改</el-button>
+                    </el-col>
                 </div>
             </el-main>
         </div>
-        
+
     </el-container>
 </template>
   
 <script>
 
 import axios from 'axios'
+import { ElMessage } from "element-plus"
 
 export default {
     name: 'Profile',
     data() {
         return {
             //"personal-info" 或 "safety-setting"
-            filter : "personal-info",
+            filter: "personal-info",
             //是否是修改模式
             modify: false,
             username: '',
             gender: '',
-            birthday: new Date(2000,0,1),
+            birthday: new Date(1900, 0, 1),
             signature: 'All done today',
             email: 'alldonetoday@163.com'
         }
@@ -118,21 +126,47 @@ export default {
             return
         }
         console.debug(response)
-        if(response.data['UserName'])
+        if (response.data['UserName'])
             this.username = response.data['UserName'];
-        if(response.data['EmailAddress'])
+        if (response.data['EmailAddress'])
             this.email = response.data['EmailAddress'];
-        if(response.data['UserGender'])
+        if (response.data['UserGender'])
             this.gender = response.data['UserGender'];
-        if(response.data['Signature'])
+        if (response.data['Signature'])
             this.signature = response.data['Signature'];
-        
-    },
-    methods: {
+        if (response.data['Birthday'])
+            this.signature = response.data['Birthday'];           
 
     },
+    methods: {
+        async update() {
+            this.modify = !this.modify
+            if(true){
+                try {
+                    await axios.post('/api/event/updateInfo', {
+                        token: localStorage.getItem('token'),
+                        UserGender: this.gender,
+                        Signature: this.signature,
+                        Birthday: this.birthday
+                    })
+                } catch (err) {
+                    ElMessage({
+                        message: '保存失败',
+                        type: 'error',
+                        grouping: true
+                    })
+                    return
+                }
+                ElMessage({
+                    message: '已保存',
+                    type: 'success',
+                    grouping: true
+                })
+            }
+        },
+    },
     computed: {
-        getBirthday(){
+        getBirthday() {
             return this.birthday.getFullYear() + '年' + (this.birthday.getMonth() + 1) + '月' + this.birthday.getDate() + '日';
         }
     }
@@ -140,39 +174,43 @@ export default {
 </script>
 
 <style scoped>
-.info-item{
+.info-item {
     display: flex;
     width: 50%;
     align-items: center;
     justify-content: flex-start;
 }
 
-.info-item-title{
+.info-item-title {
     font-weight: bold;
     color: #7a7af9;
 }
-.el-col{
+
+.el-col {
     display: flex;
     justify-content: flex-start;
 }
-.el-avatar{
+
+.el-avatar {
     margin-right: 15px;
 }
-.avatar-username{
+
+.avatar-username {
     display: flex;
     width: 100%;
     align-items: center;
     justify-content: flex-start;
     margin-bottom: 15px;
 }
-.profile-main{
+
+.profile-main {
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: flex-start;
     padding-left: 30px;
     padding-right: 30px;
-    width : 100%;
+    width: 100%;
     height: 100%;
     background-image: url("../assets/image/icon-bg.png");
     background-position: 90% 90%;
@@ -180,24 +218,28 @@ export default {
     background-repeat: no-repeat;
     user-select: none;
 }
-.profile-menu-item{
+
+.profile-menu-item {
     display: flex;
     margin: 0;
     align-items: center;
     justify-content: space-around;
     user-select: none;
 }
-.profile-menu{
+
+.profile-menu {
     width: 100%;
     height: 95%;
 }
-.profile-aside{
+
+.profile-aside {
     display: flex;
     align-items: center;
     border-radius: 10px;
     width: 20%;
     height: 100%;
 }
+
 .header-title {
     font-size: 30px;
     margin: 0;
