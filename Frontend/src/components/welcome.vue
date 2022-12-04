1ef150524c89e30ba8cea2a9aea79846ffde0834
VVsxmja <vvsxmja@qq.com>
2022-12-04 16:52:16 +0800

    添加 github 链接

--- Frontend/src/components/welcome.vue
+++ Frontend/src/components/welcome.vue
@@ -9,33 +9,33 @@ export default {
     name: 'Welcome',
     data() {
         return {
-            todolistDemo:[
-            {
-                'pic': todolist_demo1,
-                'headline':"待办清单一目了然",
-                'prompt1' :'待办事项程依次罗列，按照时间顺序分组。',
-                'prompt2' :'无论是工作计划还是备忘提醒，你都能在今日毕中便捷记录并有序规划。'
-            },
-            {
-                'pic': todolist_demo2,
-                'headline':"日程设置轻而易举",
-                'prompt1' :'轻松选择日程起止时间，设置日程重要紧急程度。',
-                'prompt2' :'添加日程详细信息，不遗漏任何要点。'
-            },
+            todolistDemo: [
+                {
+                    'pic': todolist_demo1,
+                    'headline': "待办清单一目了然",
+                    'prompt1': '待办事项程依次罗列，按照时间顺序分组。',
+                    'prompt2': '无论是工作计划还是备忘提醒，你都能在今日毕中便捷记录并有序规划。'
+                },
+                {
+                    'pic': todolist_demo2,
+                    'headline': "日程设置轻而易举",
+                    'prompt1': '轻松选择日程起止时间，设置日程重要紧急程度。',
+                    'prompt2': '添加日程详细信息，不遗漏任何要点。'
+                },
             ],
-            concentrationDemo:[
-            {
-                'pic': concentration_demo1,
-                'headline':"高效学习，心无旁骛",
-                'prompt1' :'蓬勃生长的薰衣草助力你提高专注力。',
-                'prompt2' :'快速设置专注时间，进入高效模式。'
-            },
-            {
-                'pic': concentration_demo2,
-                'headline':"专注本就如此简单",
-                'prompt1' :'专注期间全程保持全屏，阻隔其它纷扰。',
-                'prompt2' :'坚持下去，薰衣草花开之时即是胜利。'
-            },
+            concentrationDemo: [
+                {
+                    'pic': concentration_demo1,
+                    'headline': "高效学习，心无旁骛",
+                    'prompt1': '蓬勃生长的薰衣草助力你提高专注力。',
+                    'prompt2': '快速设置专注时间，进入高效模式。'
+                },
+                {
+                    'pic': concentration_demo2,
+                    'headline': "专注本就如此简单",
+                    'prompt1': '专注期间全程保持全屏，阻隔其它纷扰。',
+                    'prompt2': '坚持下去，薰衣草花开之时即是胜利。'
+                },
             ]
         }
     },
@@ -73,11 +73,11 @@ export default {
                     <div>
                         <el-container class="text-div">
                             <el-container direction="vertical">
-                                <h2 class="welcome-function-headline">{{item['headline']}}</h2>
+                                <h2 class="welcome-function-headline">{{ item['headline'] }}</h2>
                             </el-container>
                             <el-container direction="vertical" class="maintext">
-                                <p class="welcome-text">{{item['prompt1']}}</p>
-                                <p class="welcome-text">{{item['prompt2']}}</p>
+                                <p class="welcome-text">{{ item['prompt1'] }}</p>
+                                <p class="welcome-text">{{ item['prompt2'] }}</p>
                             </el-container>
                         </el-container>
                     </div>
@@ -91,11 +91,11 @@ export default {
                     <div>
                         <el-container class="text-div">
                             <el-container direction="vertical">
-                                <h2 class="welcome-function-headline">{{item['headline']}}</h2>
+                                <h2 class="welcome-function-headline">{{ item['headline'] }}</h2>
                             </el-container>
                             <el-container direction="vertical" class="maintext">
-                                <p class="welcome-text">{{item['prompt1']}}</p>
-                                <p class="welcome-text">{{item['prompt2']}}</p>
+                                <p class="welcome-text">{{ item['prompt1'] }}</p>
+                                <p class="welcome-text">{{ item['prompt2'] }}</p>
                             </el-container>
                         </el-container>
                     </div>
@@ -104,8 +104,8 @@ export default {
             </el-carousel>
         </el-container>
 
-        <el-container class="bottom-div">
-            <el-container class="text-div-bottom">
+        <el-footer class="bottom-div">
+            <el-container class="text-div-bottom" direction="vertical">
                 <el-container direction="vertical">
                     <h2 class="welcome-function-headline">今日毕贡献者</h2>
                 </el-container>
@@ -113,22 +113,36 @@ export default {
                     <p class="welcome-text"><span class="bottom-text-bold">蔡一锴</span> 环境搭建 后端 清单模式 个人信息界面 </p>
                     <p class="welcome-text"><span class="bottom-text-bold">郭桉齐</span> 注册界面 个人信息界面 </p>
                     <p class="welcome-text"><span class="bottom-text-bold">王颢瑾</span> 登录界面 后端 邮箱验证码 </p>
-                    <p class="welcome-text"><span class="bottom-text-bold">郑博远</span> UI设计 专注模式 清单模式 欢迎页  </p>
+                    <p class="welcome-text"><span class="bottom-text-bold">郑博远</span> UI设计 专注模式 清单模式 欢迎页 </p>
+                    <div>
+                        <el-link href="https://github.com/VVsxmja/ToDoList" type="primary" class="welcome-link" :underline="false">
+                            <el-icon size="35" style="margin: 5px;">
+                                <svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1.2em"
+                                    height="1.2em" data-v-6c8d2bba="">
+                                    <path fill="currentColor"
+                                        d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476c0-.237-.013-1.024-.013-1.862c-2.512.463-3.162-.612-3.362-1.175c-.113-.288-.6-1.175-1.025-1.413c-.35-.187-.85-.65-.013-.662c.788-.013 1.35.725 1.538 1.025c.9 1.512 2.338 1.087 2.912.825c.088-.65.35-1.087.638-1.337c-2.225-.25-4.55-1.113-4.55-4.938c0-1.088.387-1.987 1.025-2.688c-.1-.25-.45-1.275.1-2.65c0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337c1.912-1.3 2.75-1.024 2.75-1.024c.55 1.375.2 2.4.1 2.65c.637.7 1.025 1.587 1.025 2.687c0 3.838-2.337 4.688-4.562 4.938c.362.312.675.912.675 1.85c0 1.337-.013 2.412-.013 2.75c0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z">
+                                    </path>
+                                </svg>
+                            </el-icon>
+                            &nbsp;
+                            <p>在 GitHub 上查看源代码</p>
+                        </el-link>
+                    </div>
                 </el-container>
             </el-container>
-        </el-container>
+        </el-footer>
     </el-container>
 </template>
 
 
 <style scoped>
-
-.bottom-text-bold{
+.bottom-text-bold {
     font-weight: bold;
     color: #7a7af9;
     margin-right: 10px;
 }
-.el-carousel{
+
+.el-carousel {
     width: 100%;
     max-width: 1280px;
     background-color: #efefff;
@@ -250,6 +264,11 @@ export default {
     font-size: 20px;
     font-weight: bold;
     margin-right: 20px;
+}
+
+.welcome-link {
+    width: 300px;
+    margin-top: 30px;
 }
 </style>
   
