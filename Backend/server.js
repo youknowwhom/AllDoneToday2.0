ddaa8e06c0bc311cda85167cda1bdac88dcfa919
GGGaq <108728800+GGGaq@users.noreply.github.com>
2023-06-09 09:02:08 +0800

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,11 +7,7 @@ const app = express()
 import crypto from 'crypto'
 
 async function sha256(message) {
-    const msgBuffer = new TextEncoder().encode(message)
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer)
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
-    return hashHex
+    return crypto.createHash('sha256').update(message).digest('hex')
 }
 
 /**
